5755132cba5acdc29233d8a92b54fb7d7d2ae029
Hoang Anh Luong <vinaast1220@gmail.com>
2023-10-05 14:46:15 +0700

    Token publish

--- src/main/java/com/example/FAMS/dto/responses/LoginResponse.java
+++ src/main/java/com/example/FAMS/dto/responses/LoginResponse.java
@@ -12,5 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginResponse {
     private String status;
+    private String token;
     private UserDTO userInfo;
 }
--- src/main/java/com/example/FAMS/service_implementors/AuthenticationServiceImpl.java
+++ src/main/java/com/example/FAMS/service_implementors/AuthenticationServiceImpl.java
@@ -52,6 +52,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         return LoginResponse.builder()
 //                Need something here
                 .status("Successful")
+                .token(token)
                 .userInfo(userDAO.findUserByEmail(loginRequest.getEmail()).orElse(null))
                 .build();
     }
