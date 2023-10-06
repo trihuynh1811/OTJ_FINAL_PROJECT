71e23f46a73ccf44dec3e7d619ea53c6d5558031
ThanhPhong383 <37406424+ThanhPhong383@users.noreply.github.com>
2023-10-06 10:16:57 +0700

    ROLE TRAINER, CLASS_ADMIN, USER

--- src/main/java/com/example/FAMS/FamsApplication.java
+++ src/main/java/com/example/FAMS/FamsApplication.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Date;
 import java.util.List;
 
@@ -26,80 +25,117 @@ import static com.example.FAMS.enums.Permission.*;
 @RequestMapping(path = "/")
 public class FamsApplication {
 
-	private final UserDAO userDAO;
-    private final UserPermissionDAO userPermissionDAO;
-	private final PasswordEncoder passwordEncoder;
+  private final UserDAO userDAO;
+  private final UserPermissionDAO userPermissionDAO;
+  private final PasswordEncoder passwordEncoder;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FamsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FamsApplication.class, args);
+  }
 
+  @Bean
+  public CommandLineRunner initData() {
+    return new CommandLineRunner() {
+      @Override
+      public void run(String... args) throws Exception {
+        UserPermission superAdminPermission =
+            UserPermission.builder()
+                .role(Role.SUPER_ADMIN)
+                .syllabus(
+                    List.of(
+                        SYLLABUS_CREATE,
+                        SYLLABUS_READ,
+                        SYLLABUS_UPDATE,
+                        SYLLABUS_DELETE,
+                        SYLLABUS_IMPORT))
+                .trainingProgram(
+                    List.of(
+                        TRAINING_CREATE,
+                        TRAINING_READ,
+                        TRAINING_UPDATE,
+                        TRAINING_DELETE,
+                        TRAINING_IMPORT))
+                .userClass(
+                    List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
+                .userManagement(
+                    List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
+                .learningMaterial(List.of())
+                .build();
 
-    @Bean
-    public CommandLineRunner initData() {
-        return new CommandLineRunner() {
-			@Override
-            public void run(String... args) throws Exception {
-                UserPermission superAdminPermission = UserPermission.builder()
-                        .role(Role.SUPER_ADMIN)
-                        .syllabus(List.of(
-                                SYLLABUS_CREATE,
-                                SYLLABUS_READ,
-                                SYLLABUS_UPDATE,
-                                SYLLABUS_DELETE,
-                                SYLLABUS_IMPORT
-                        ))
-                        .trainingProgram(List.of(
-                                TRAINING_CREATE,
-                                TRAINING_READ,
-                                TRAINING_UPDATE,
-                                TRAINING_DELETE,
-                                TRAINING_IMPORT
-                        ))
-                        .userClass(List.of(
-                                CLASS_CREATE,
-                                CLASS_READ,
-                                CLASS_UPDATE,
-                                CLASS_DELETE,
-                                CLASS_IMPORT
-                        ))
-                        .userManagement(List.of(
-                                USER_CREATE,
-                                USER_READ,
-                                USER_UPDATE,
-                                USER_DELETE,
-                                USER_IMPORT
-                        ))
-                        .learningMaterial(List.of())
-                        .build();
-                try {
-                    userPermissionDAO.save(superAdminPermission);
-                } catch (Exception e) {
-                    System.out.println("ERROR: " + e.getMessage());
-                }
-                var permission = userPermissionDAO.findUserPermissionByRole(Role.SUPER_ADMIN)
-                        .orElse(null);
-                User admin = User.builder()
-                        .email("admin@gmail.com")
-                        .password(passwordEncoder.encode("1"))
-                        .name("Admin")
-                        .phone("0977545450")
-                        .dob(new Date())
-                        .gender("Male")
-                        .role(permission)
-                        .status("Wonderful")
-                        .createdBy("Hoang Anh")
-                        .createdDate(new Date())
-                        .modifiedBy("Hoang Anh")
-                        .modifiedDate(new Date())
-                        .build();
-                userDAO.save(admin);
-            }
-        };
-    }
+        try {
+          userPermissionDAO.save(superAdminPermission);
+        } catch (Exception e) {
+          System.out.println("ERROR: " + e.getMessage());
+        }
+        UserPermission trainerPermission =
+            UserPermission.builder()
+                .role(Role.TRAINER)
+                .syllabus(List.of(SYLLABUS_CREATE, SYLLABUS_READ, SYLLABUS_UPDATE, SYLLABUS_DELETE))
+                .trainingProgram(List.of(TRAINING_READ))
+                .userClass(List.of(CLASS_READ))
+                .build();
+        try {
+          userPermissionDAO.save(trainerPermission);
+        } catch (Exception e) {
+          System.out.println("ERROR: " + e.getMessage());
+        }
+        UserPermission userPermission =
+            UserPermission.builder().role(Role.USER).userManagement(List.of(USER_READ)).build();
+        try {
+          userPermissionDAO.save(userPermission);
+        } catch (Exception e) {
+          System.out.println("ERROR: " + e.getMessage());
+        }
+        UserPermission classAdminPermission =
+            UserPermission.builder()
+                .role(Role.CLASS_ADMIN)
+                .syllabus(
+                    List.of(
+                        SYLLABUS_CREATE,
+                        SYLLABUS_READ,
+                        SYLLABUS_UPDATE,
+                        SYLLABUS_DELETE,
+                        SYLLABUS_IMPORT))
+                .trainingProgram(
+                    List.of(
+                        TRAINING_CREATE,
+                        TRAINING_READ,
+                        TRAINING_UPDATE,
+                        TRAINING_DELETE,
+                        TRAINING_IMPORT))
+                .userClass(
+                    List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
+                .userManagement(
+                    List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
+                .build();
+        try {
+          userPermissionDAO.save(classAdminPermission);
+        } catch (Exception e) {
+          System.out.println("ERROR: " + e.getMessage());
+        }
+        var permission = userPermissionDAO.findUserPermissionByRole(Role.SUPER_ADMIN).orElse(null);
+        User admin =
+            User.builder()
+                .email("admin@gmail.com")
+                .password(passwordEncoder.encode("1"))
+                .name("Admin")
+                .phone("0977545450")
+                .dob(new Date())
+                .gender("Male")
+                .role(permission)
+                .status("Wonderful")
+                .createdBy("Hoang Anh")
+                .createdDate(new Date())
+                .modifiedBy("Hoang Anh")
+                .modifiedDate(new Date())
+                .build();
+        userDAO.save(admin);
+      }
+    };
+  }
 
-    @GetMapping("")
-    public String greeting() {
-        return "Hello from FAMS Application made from GROUP 1 WITH LOVE";
-    }
+  @GetMapping("")
+  public String greeting() {
+    return "Hello from FAMS Application made from GROUP 1 WITH LOVE";
+  }
 }
