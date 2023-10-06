88c24a22092952abf686ca626b9f44a810b2e555
Hoang Anh Luong <vinaast1220@gmail.com>
2023-10-06 21:36:45 +0700

    Update permission

--- src/main/java/com/example/FAMS/FamsApplication.java
+++ src/main/java/com/example/FAMS/FamsApplication.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -25,180 +27,131 @@ import static com.example.FAMS.enums.Permission.*;
 @RequestMapping(path = "/")
 public class FamsApplication {
 
-  private final UserDAO userDAO;
-  private final UserPermissionDAO userPermissionDAO;
-  private final PasswordEncoder passwordEncoder;
+    private final UserDAO userDAO;
+    private final UserPermissionDAO userPermissionDAO;
+    private final PasswordEncoder passwordEncoder;
 
-  public static void main(String[] args) {
-    SpringApplication.run(FamsApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(FamsApplication.class, args);
+    }
 
-  @Bean
-  public CommandLineRunner initData() {
-    return new CommandLineRunner() {
-      @Override
-      public void run(String... args) throws Exception {
-        UserPermission superAdminPermission =
-            UserPermission.builder()
-                .role(Role.SUPER_ADMIN)
-                .syllabus(
-                    List.of(
-                        SYLLABUS_CREATE,
-                        SYLLABUS_READ,
-                        SYLLABUS_UPDATE,
-                        SYLLABUS_DELETE,
-                        SYLLABUS_IMPORT))
-                .trainingProgram(
-                    List.of(
-                        TRAINING_CREATE,
-                        TRAINING_READ,
-                        TRAINING_UPDATE,
-                        TRAINING_DELETE,
-                        TRAINING_IMPORT))
-                .userClass(
-                    List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
-                .userManagement(
-                    List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
-                .learningMaterial(List.of())
-                .build();
+    @Bean
+    public CommandLineRunner initData() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                List<UserPermission> permissionList = new ArrayList<>();
+                List<User> userList = new ArrayList<>();
 
-        try {
-          userPermissionDAO.save(superAdminPermission);
-        } catch (Exception e) {
-          System.out.println("ERROR: " + e.getMessage());
-        }
-        UserPermission trainerPermission =
-            UserPermission.builder()
-                .role(Role.TRAINER)
-                .syllabus(List.of(SYLLABUS_CREATE, SYLLABUS_READ, SYLLABUS_UPDATE, SYLLABUS_DELETE))
-                .trainingProgram(List.of(TRAINING_READ))
-                .userClass(List.of(CLASS_READ))
-                .userManagement(List.of())
-                .learningMaterial(List.of())
-                .build();
-        try {
-          userPermissionDAO.save(trainerPermission);
-        } catch (Exception e) {
-          System.out.println("ERROR: " + e.getMessage());
-        }
-        UserPermission userPermission =
-            UserPermission.builder()
-                .role(Role.USER)
-                .syllabus(List.of())
-                .trainingProgram(List.of())
-                .userClass(List.of())
-                .userManagement(List.of(USER_READ))
-                .learningMaterial(List.of())
-                .build();
-        try {
-          userPermissionDAO.save(userPermission);
-        } catch (Exception e) {
-          System.out.println("ERROR: " + e.getMessage());
-        }
-        UserPermission classAdminPermission =
-            UserPermission.builder()
-                .role(Role.CLASS_ADMIN)
-                .syllabus(
-                    List.of(
-                        SYLLABUS_CREATE,
-                        SYLLABUS_READ,
-                        SYLLABUS_UPDATE,
-                        SYLLABUS_DELETE,
-                        SYLLABUS_IMPORT))
-                .trainingProgram(
-                    List.of(
-                        TRAINING_CREATE,
-                        TRAINING_READ,
-                        TRAINING_UPDATE,
-                        TRAINING_DELETE,
-                        TRAINING_IMPORT))
-                .userClass(
-                    List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
-                .userManagement(
-                    List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
-                .learningMaterial(List.of())
-                .build();
-        try {
-          userPermissionDAO.save(classAdminPermission);
-        } catch (Exception e) {
-          System.out.println("ERROR: " + e.getMessage());
-        }
-        var permission = userPermissionDAO.findUserPermissionByRole(Role.SUPER_ADMIN).orElse(null);
-        User admin =
-            User.builder()
-                .email("admin@gmail.com")
-                .password(passwordEncoder.encode("1"))
-                .name("Admin")
-                .phone("0977545450")
-                .dob(new Date())
-                .gender("Male")
-                .role(permission)
-                .status("Wonderful")
-                .createdBy("Hoang Anh")
-                .createdDate(new Date())
-                .modifiedBy("Hoang Anh")
-                .modifiedDate(new Date())
-                .build();
-        userDAO.save(admin);
+                permissionList.add(UserPermission.builder()
+                        .role(Role.SUPER_ADMIN)
+                        .syllabus(
+                                List.of(
+                                        SYLLABUS_CREATE, SYLLABUS_READ, SYLLABUS_UPDATE, SYLLABUS_DELETE, SYLLABUS_IMPORT))
+                        .trainingProgram(
+                                List.of(
+                                        TRAINING_CREATE, TRAINING_READ, TRAINING_UPDATE, TRAINING_DELETE, TRAINING_IMPORT))
+                        .userClass(
+                                List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
+                        .userManagement(
+                                List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
+                        .learningMaterial(List.of())
+                        .build());
+                permissionList.add(UserPermission.builder()
+                        .role(Role.TRAINER)
+                        .syllabus(List.of(SYLLABUS_CREATE, SYLLABUS_READ, SYLLABUS_UPDATE, SYLLABUS_DELETE))
+                        .trainingProgram(List.of(TRAINING_READ))
+                        .userClass(List.of(CLASS_READ))
+                        .userManagement(List.of())
+                        .learningMaterial(List.of())
+                        .build());
+                permissionList.add(UserPermission.builder()
+                        .role(Role.USER)
+                        .syllabus(List.of())
+                        .trainingProgram(List.of())
+                        .userClass(List.of())
+                        .userManagement(List.of(USER_READ))
+                        .learningMaterial(List.of())
+                        .build());
+                permissionList.add(UserPermission.builder()
+                        .role(Role.CLASS_ADMIN)
+                        .syllabus(
+                                List.of(
+                                        SYLLABUS_CREATE, SYLLABUS_READ, SYLLABUS_UPDATE, SYLLABUS_DELETE, SYLLABUS_IMPORT))
+                        .trainingProgram(
+                                List.of(
+                                        TRAINING_CREATE, TRAINING_READ, TRAINING_UPDATE, TRAINING_DELETE, TRAINING_IMPORT))
+                        .userClass(
+                                List.of(CLASS_CREATE, CLASS_READ, CLASS_UPDATE, CLASS_DELETE, CLASS_IMPORT))
+                        .userManagement(
+                                List.of(USER_CREATE, USER_READ, USER_UPDATE, USER_DELETE, USER_IMPORT))
+                        .learningMaterial(List.of())
+                        .build());
+                userPermissionDAO.saveAll(permissionList);
 
-        var classAdminRole =
-            userPermissionDAO.findUserPermissionByRole(Role.CLASS_ADMIN).orElse(null);
-        User classAdmin =
-            User.builder()
-                .email("classadmin@gmail.com")
-                .password(passwordEncoder.encode("1"))
-                .name("Class Admin")
-                .phone("0977545451")
-                .dob(new Date())
-                .gender("Male")
-                .role(classAdminRole)
-                .status("Wonderful")
-                .createdBy("Hoang Anh")
-                .createdDate(new Date())
-                .modifiedBy("Hoang Anh")
-                .modifiedDate(new Date())
-                .build();
-        userDAO.save(classAdmin);
-        var trainerRole = userPermissionDAO.findUserPermissionByRole(Role.TRAINER).orElse(null);
-        User trainer =
-            User.builder()
-                .email("trainer@gmail.com")
-                .password(passwordEncoder.encode("1"))
-                .name("Trainer")
-                .phone("0977545452")
-                .dob(new Date())
-                .gender("Male")
-                .role(trainerRole)
-                .status("Wonderful")
-                .createdBy("Hoang Anh")
-                .createdDate(new Date())
-                .modifiedBy("Hoang Anh")
-                .modifiedDate(new Date())
-                .build();
-        userDAO.save(trainer);
-        var userRole = userPermissionDAO.findUserPermissionByRole(Role.USER).orElse(null);
-        User user =
-            User.builder()
-                .email("user@gmail.com")
-                .password(passwordEncoder.encode("1"))
-                .name("User")
-                .phone("0977545453")
-                .dob(new Date())
-                .gender("Male")
-                .role(userRole)
-                .status("Wonderful")
-                .createdBy("Hoang Anh")
-                .createdDate(new Date())
-                .modifiedBy("Hoang Anh")
-                .modifiedDate(new Date())
-                .build();
-        userDAO.save(user);
-      }
-    };
-  }
+                userList.add(User.builder()
+                        .email("admin@gmail.com")
+                        .password(passwordEncoder.encode("1"))
+                        .name("Admin")
+                        .phone("0977545450")
+                        .dob(new Date())
+                        .gender("Male")
+                        .role(userPermissionDAO.findUserPermissionByRole(Role.SUPER_ADMIN).orElse(null))
+                        .status("Wonderful")
+                        .createdBy("Hoang Anh")
+                        .createdDate(new Date())
+                        .modifiedBy("Hoang Anh")
+                        .modifiedDate(new Date())
+                        .build());
+                userList.add(User.builder()
+                        .email("classadmin@gmail.com")
+                        .password(passwordEncoder.encode("1"))
+                        .name("Class Admin")
+                        .phone("0977545451")
+                        .dob(new Date())
+                        .gender("Male")
+                        .role(userPermissionDAO.findUserPermissionByRole(Role.CLASS_ADMIN).orElse(null))
+                        .status("Wonderful")
+                        .createdBy("Hoang Anh")
+                        .createdDate(new Date())
+                        .modifiedBy("Hoang Anh")
+                        .modifiedDate(new Date())
+                        .build());
+                userList.add(User.builder()
+                        .email("trainer@gmail.com")
+                        .password(passwordEncoder.encode("1"))
+                        .name("Trainer")
+                        .phone("0977545452")
+                        .dob(new Date())
+                        .gender("Male")
+                        .role(userPermissionDAO.findUserPermissionByRole(Role.TRAINER).orElse(null))
+                        .status("Wonderful")
+                        .createdBy("Hoang Anh")
+                        .createdDate(new Date())
+                        .modifiedBy("Hoang Anh")
+                        .modifiedDate(new Date())
+                        .build());
+                userList.add(User.builder()
+                        .email("user@gmail.com")
+                        .password(passwordEncoder.encode("1"))
+                        .name("User")
+                        .phone("0977545453")
+                        .dob(new Date())
+                        .gender("Male")
+                        .role(userPermissionDAO.findUserPermissionByRole(Role.USER).orElse(null))
+                        .status("Wonderful")
+                        .createdBy("Hoang Anh")
+                        .createdDate(new Date())
+                        .modifiedBy("Hoang Anh")
+                        .modifiedDate(new Date())
+                        .build());
+                userDAO.saveAll(userList);
+            }
+        };
+    }
 
-  @GetMapping("")
-  public String greeting() {
-    return "Hello from FAMS Application made from GROUP 1 WITH LOVE";
-  }
+    @GetMapping("")
+    public String greeting() {
+        return "Hello from FAMS Application made from GROUP 1 WITH LOVE";
+    }
 }
