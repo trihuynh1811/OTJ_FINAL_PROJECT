9d2707d2b7351d02714146ca74023a7ccaed332a
ThanhPhong383 <37406424+ThanhPhong383@users.noreply.github.com>
2023-10-19 14:16:33 +0700

    Class

--- src/main/java/com/example/FAMS/service_implementors/ClassServiceImpl.java
+++ src/main/java/com/example/FAMS/service_implementors/ClassServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.FAMS.dto.requests.UpdateClassRequest;
 import com.example.FAMS.dto.responses.UpdateClassResponse;
 import com.example.FAMS.models.Class;
 import com.example.FAMS.models.ClassLearningDay; // Thêm ClassLearningDay
+import com.example.FAMS.models.ClassUser;
+import com.example.FAMS.models.User;
 import com.example.FAMS.repositories.ClassDAO;
 import com.example.FAMS.repositories.TrainingProgramDAO;
 import com.example.FAMS.services.ClassService;
@@ -22,137 +24,159 @@ import java.util.stream.Collectors;
 @Service
 public class ClassServiceImpl implements ClassService {
 
-    @Autowired
-    ClassDAO classDAO;
-    TrainingProgramDAO trainingProgramDAO;
+  @Autowired ClassDAO classDAO;
+  TrainingProgramDAO trainingProgramDAO;
 
-    @Override
-    public List<Class> getClasses() {
-        return classDAO.findTop1000ByOrderByCreatedDateDesc();
+  @Override
+  public List<Class> getClasses() {
+    return classDAO.findTop1000ByOrderByCreatedDateDesc();
+  }
+
+  @Override
+  public List<Class> getDetailClass() {
+    return classDAO.findAll();
+  }
+
+  @Override
+  public Class createClass(
+      String className,
+      String classCode,
+      String duration,
+      String location,
+      Date startDate,
+      Date endDate,
+      String createdBy,
+      Set<ClassLearningDay> learningDays,
+      Set<ClassUser> classUsers,
+      User user) {
+    // Kiểm tra điều kiện cho className, classCode, duration, location, startDate, endDate,
+    // createdBy
+    if (className == null
+        || classCode == null
+        || duration == null
+        || location == null
+        || startDate == null
+        || endDate == null
+        || createdBy == null) {
+      throw new IllegalArgumentException("All fields are required.");
     }
 
-    @Override
-    public List<Class> getDetailClass() {
-        return classDAO.findAll();
+    // Kiểm tra điều kiện cho learningDays
+    if (learningDays == null || learningDays.isEmpty()) {
+      throw new IllegalArgumentException("At least one learning day is required.");
     }
 
-    @Override
-    public Class createClass(String className, String classCode, String duration, String location, Date startDate, Date endDate, String createdBy) {
-        Class classInfo = Class.builder()
-                .className(className)
-                .classCode(classCode)
-                .duration(duration)
-                .location(location)
-                .startDate(startDate)
-                .endDate(endDate)
-                .createdBy(createdBy)
-                .build();
-
-        classDAO.save(classInfo);
-        return classInfo;
+    // Kiểm tra điều kiện cho user: vai trò TRAINER
+    if (user != null && !"TRAINER".equals(user.getRole().getRole().name())) {
+      throw new IllegalArgumentException("The user must have the TRAINER role.");
     }
 
-    // Thêm phương thức tạo lớp học với danh sách ngày học
-    @Override
-    public Class createClassWithLearningDays(
-            String className, String classCode, String duration, String location, Date startDate, Date endDate, String createdBy, Set<ClassLearningDay> learningDays
-    ) {
-        Class classInfo = Class.builder()
-                .className(className)
-                .classCode(classCode)
-                .duration(duration)
-                .location(location)
-                .startDate(startDate)
-                .endDate(endDate)
-                .createdBy(createdBy)
-                .classLearningDays(learningDays) // Đặt danh sách ngày học
-                .build();
+    Class classInfo =
+        Class.builder()
+            .className(className)
+            .classCode(classCode)
+            .duration(duration)
+            .location(location)
+            .startDate(startDate)
+            .endDate(endDate)
+            .createdBy(createdBy)
+            .classLearningDays(learningDays)
+            .classUsers(classUsers)
+            .build();
 
-        classDAO.save(classInfo);
-        return classInfo;
+    classDAO.save(classInfo);
+    return classInfo;
+  }
+
+  @Override
+  public UpdateClassResponse updateClass(UpdateClassRequest updateClassRequest) {
+    Optional<Class> optionalClass = classDAO.findById(updateClassRequest.getClassId());
+    Class existingClass = optionalClass.orElse(null);
+    if (existingClass != null) {
+      existingClass =
+          Class.builder()
+              .classId(existingClass.getClassId())
+              .className(updateClassRequest.getClassName())
+              .classCode(updateClassRequest.getClassCode())
+              .duration(updateClassRequest.getDuration())
+              .location(updateClassRequest.getLocation())
+              .startDate(updateClassRequest.getStartDate())
+              .endDate(updateClassRequest.getEndDate())
+              .createdBy(existingClass.getCreatedBy())
+              .createdDate(existingClass.getCreatedDate())
+              .modifiedBy(existingClass.getModifiedBy())
+              .modifiedDate(existingClass.getModifiedDate())
+              .classLearningDays(
+                  existingClass.getClassLearningDays()) // Bổ sung cập nhật danh sách ngày học
+              .build();
+
+      Class updatedClass = classDAO.save(existingClass);
+
+      if (updatedClass != null) {
+        return UpdateClassResponse.builder()
+            .status("Update Class successful")
+            .updatedClass(updatedClass)
+            .build();
+      } else {
+        return UpdateClassResponse.builder()
+            .status("Update Class failed")
+            .updatedClass(null)
+            .build();
+      }
+    } else {
+      return UpdateClassResponse.builder().status("Class not found").updatedClass(null).build();
+    }
+  }
+
+  public List<Class> getDetailClasses() {
+    return classDAO.findAll();
+  }
+
+  @Override
+  public Class getClassById(int classId) {
+    Optional<Class> optionalClass = classDAO.findById(classId);
+    return optionalClass.orElse(null);
+  }
+
+  @Override
+  public List<Class> getAll() {
+    return classDAO.getAll();
+  }
+
+  public List<Class> searchClass(String createdDate, String searchValue, String orderBy) {
+    List<Class> classList = classDAO.findAll();
+
+    if (!Strings.isNullOrEmpty(createdDate)) {
+      classList =
+          classList.stream()
+              .filter(
+                  c ->
+                      new SimpleDateFormat("yyyy-MM-dd")
+                          .format(c.getCreatedDate())
+                          .equals(createdDate))
+              .collect(Collectors.toList());
     }
 
-    @Override
-    public UpdateClassResponse updateClass(UpdateClassRequest updateClassRequest) {
-        Optional<Class> optionalClass = classDAO.findById(updateClassRequest.getClassId());
-        Class existingClass = optionalClass.orElse(null);
-        if (existingClass != null) {
-            existingClass =
-                    Class.builder()
-                            .classId(existingClass.getClassId())
-                            .className(updateClassRequest.getClassName())
-                            .classCode(updateClassRequest.getClassCode())
-                            .duration(updateClassRequest.getDuration())
-                            .location(updateClassRequest.getLocation())
-                            .startDate(updateClassRequest.getStartDate())
-                            .endDate(updateClassRequest.getEndDate())
-                            .createdBy(existingClass.getCreatedBy())
-                            .createdDate(existingClass.getCreatedDate())
-                            .modifiedBy(existingClass.getModifiedBy())
-                            .modifiedDate(existingClass.getModifiedDate())
-                            .classLearningDays(existingClass.getClassLearningDays()) // Bổ sung cập nhật danh sách ngày học
-                            .build();
-
-            Class updatedClass = classDAO.save(existingClass);
-
-            if (updatedClass != null) {
-                return UpdateClassResponse.builder()
-                        .status("Update Class successful")
-                        .updatedClass(updatedClass)
-                        .build();
-            } else {
-                return UpdateClassResponse.builder()
-                        .status("Update Class failed")
-                        .updatedClass(null)
-                        .build();
-            }
-        } else {
-            return UpdateClassResponse.builder()
-                    .status("Class not found")
-                    .updatedClass(null)
-                    .build();
-        }
+    if (!Strings.isNullOrEmpty(searchValue)) {
+      classList =
+          classList.stream()
+              .filter(
+                  c ->
+                      c.getClassName().toLowerCase().contains(searchValue.trim().toLowerCase())
+                          || c.getClassCode()
+                              .toLowerCase()
+                              .contains(searchValue.trim().toLowerCase()))
+              .collect(Collectors.toList());
     }
 
-    public List<Class> getDetailClasses() {
-        return classDAO.findAll();
+    if (!Strings.isNullOrEmpty(orderBy)) {
+      if (orderBy.equals("asc")) {
+        classList.sort(Comparator.comparing(Class::getClassName));
+      } else if (orderBy.equals("desc")) {
+        classList.sort(Comparator.comparing(Class::getClassName).reversed());
+      }
     }
 
-    @Override
-    public Class getClassById(int classId) {
-        Optional<Class> optionalClass = classDAO.findById(classId);
-        return optionalClass.orElse(null);
-    }
-
-    @Override
-    public List<Class> getAll() {
-        return classDAO.getAll();
-    }
-
-    public List<Class> searchClass(String createdDate, String searchValue, String orderBy) {
-        List<Class> classList = classDAO.findAll();
-
-        if (!Strings.isNullOrEmpty(createdDate)) {
-            classList = classList.stream()
-                    .filter(c -> new SimpleDateFormat("yyyy-MM-dd").format(c.getCreatedDate()).equals(createdDate))
-                    .collect(Collectors.toList());
-        }
-
-        if (!Strings.isNullOrEmpty(searchValue)) {
-            classList = classList.stream()
-                    .filter(c -> c.getClassName().toLowerCase().contains(searchValue.trim().toLowerCase())
-                            || c.getClassCode().toLowerCase().contains(searchValue.trim().toLowerCase()))
-                    .collect(Collectors.toList());
-        }
-
-        if (!Strings.isNullOrEmpty(orderBy)) {
-            if (orderBy.equals("asc")) {
-                classList.sort(Comparator.comparing(Class::getClassName));
-            } else if (orderBy.equals("desc")) {
-                classList.sort(Comparator.comparing(Class::getClassName).reversed());
-            }
-        }
-
-        return classList;
-    }
+    return classList;
+  }
 }
--- src/main/java/com/example/FAMS/services/ClassService.java
+++ src/main/java/com/example/FAMS/services/ClassService.java
@@ -2,36 +2,33 @@ package com.example.FAMS.services;
 
 import com.example.FAMS.dto.requests.UpdateClassRequest;
 import com.example.FAMS.dto.responses.UpdateClassResponse;
+import com.example.FAMS.models.*;
 import com.example.FAMS.models.Class;
-import com.example.FAMS.models.ClassLearningDay;
-import com.example.FAMS.models.Syllabus;
-import com.example.FAMS.models.TrainingProgram;
 
 import java.sql.Date;
 import java.util.List;
 import java.util.Set;
 
 public interface ClassService {
-    List<Class> getClasses();
+  List<Class> getClasses();
 
-    List<Class> getDetailClass();
+  List<Class> getDetailClass();
 
-    Class createClass(String className, String classCode, String duration, String location, Date startDate, Date endDate, String createdBy);
+  public Class createClass(
+      String className,
+      String classCode,
+      String duration,
+      String location,
+      Date startDate,
+      Date endDate,
+      String createdBy,
+      Set<ClassLearningDay> learningDays,
+      Set<ClassUser> classUsers,
+      User user);
 
-    // Thêm phương thức tạo lớp học với danh sách ngày học
-    Class createClassWithLearningDays(
-            String className, String classCode, String duration, String location, Date startDate, Date endDate, String createdBy, Set<ClassLearningDay> learningDays
-    );
+  UpdateClassResponse updateClass(UpdateClassRequest updateClassRequest);
 
-    UpdateClassResponse updateClass(UpdateClassRequest updateClassRequest);
+  Class getClassById(int classId);
 
-
-
-
-
-
-
-
-    Class getClassById(int classId);
-
-    List<Class> getAll();}
+  List<Class> getAll();
+}
