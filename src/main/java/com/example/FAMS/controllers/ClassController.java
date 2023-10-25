686f4d0bf956c84fd712c00aa9b5aecbc31e1056
trihuynh <77220677+trihuynh1811@users.noreply.github.com>
2023-10-25 10:46:56 +0700

    mock for class function

--- src/main/java/com/example/FAMS/controllers/ClassController.java
+++ src/main/java/com/example/FAMS/controllers/ClassController.java
@@ -1,22 +1,23 @@
 package com.example.FAMS.controllers;
 
 import com.example.FAMS.dto.requests.Calendar.UpdateCalendarRequest;
-import com.example.FAMS.dto.requests.UpdateClassRequest;
+import com.example.FAMS.dto.responses.ClassRes.GetAllClasses;
+import com.example.FAMS.dto.responses.ClassRes.GetClass;
+import com.example.FAMS.dto.responses.ClassRes.GetClassDetail;
 import com.example.FAMS.dto.responses.ResponseObject;
 import com.example.FAMS.dto.responses.UpdateCalendarResponse;
-import com.example.FAMS.dto.responses.UpdateClassResponse;
-import com.example.FAMS.models.Class;
+import com.example.FAMS.models.Syllabus;
 import com.example.FAMS.service_implementors.ClassServiceImpl;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -30,21 +31,21 @@ public class ClassController {
 
     @GetMapping
     @PreAuthorize("hasAuthority('class:read')")
-    public ResponseEntity<String> getClasses() {
-        return ResponseEntity.status(HttpStatus.OK).body("a list of class have been returned.");
+    public ResponseEntity<List<GetAllClasses>> getClasses() {
+        List<GetAllClasses> classes = getListClasses();
+        return ResponseEntity.status(HttpStatus.OK).body(classes);
     }
 
     @GetMapping("/detail")
     @PreAuthorize("hasAuthority('class:read')")
-    public ResponseEntity<String> getDetailClasses() {
-        return ResponseEntity.ok("details of a class have been returned.");
+    public ResponseEntity<GetClassDetail> getDetailClasses() {
+        return ResponseEntity.ok(detailOfClass());
     }
 
     @PostMapping("/create")
     @PreAuthorize("hasAuthority('class:create')")
-    public ResponseEntity<String> createClass(){
-
-        return ResponseEntity.status(HttpStatus.OK).body("successfully create a class.");
+    public ResponseEntity<GetClassDetail> createClass(){
+        return ResponseEntity.status(HttpStatus.OK).body(detailOfClass());
     }
 
     @PutMapping("/update/{classId}")
@@ -52,14 +53,14 @@ public class ClassController {
         return ResponseEntity.status(200).body("successfully update class with id: " + classId);
     }
 
+    @PutMapping("/delete/{classId}")
+    public ResponseEntity<String> deleteClass(@PathVariable String classId) {
+        return ResponseEntity.status(200).body("successfully delete class with id: " + classId);
+    }
+
     @GetMapping("/search/{classId}")
-    public ResponseEntity<?> getClassById(@PathVariable int classId) {
-        Class classInfo = classService.getClassById(classId);
-        if (classInfo != null) {
-            return ResponseEntity.ok(classInfo);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<GetClass> getClassById(@PathVariable String classId) {
+        return ResponseEntity.status(200).body(getClass_(classId));
     }
 
     @GetMapping("/listClass")
@@ -94,4 +95,89 @@ public class ClassController {
     public UpdateCalendarResponse updateClassLearningDay(@RequestBody UpdateCalendarRequest request) throws ParseException {
         return classService.updateClassLearningDay(request);
     }
+
+    public List<GetAllClasses> getListClasses(){
+        List<GetAllClasses> classes = new ArrayList<>();
+        for(int i = 0; i < 5; i ++){
+            GetAllClasses class_ = GetAllClasses.builder()
+                    .className("class " + i)
+                    .classCode(Integer.toString(i))
+                    .createdOn(new Date().getTime())
+                    .fsu("fhcm")
+                    .status("planing")
+                    .createBy("joe mama")
+                    .location("joe mama house")
+                    .duration(69)
+                    .build();
+
+            classes.add(class_);
+        }
+        return classes;
+    }
+
+    public GetClassDetail detailOfClass(){
+        List<Syllabus> syllabusList = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            Syllabus s = Syllabus.builder()
+                    .topicCode("mock topic " + i)
+                    .topicName("topic " + i)
+                    .topicOutline("lorem...")
+                    .courseObjective("lorem...")
+                    .modifiedBy("joe father")
+                    .modifiedDate(new Date())
+                    .version("1.0")
+                    .trainingMaterials("nothing")
+                    .trainingPrinciples("idk")
+                    .publishStatus("active")
+                    .createdBy("joe")
+                    .createdDate(new Date())
+                    .technicalGroup("a pc")
+                    .trainingAudience(100)
+                    .numberOfDay(2000)
+                    .priority("high")
+                    .build();
+
+            syllabusList.add(s);
+        }
+
+        GetClassDetail class_ = GetClassDetail.builder()
+                .className("class " + 0)
+                .classCode(Integer.toString(0))
+                .createdOn(new Date().getTime())
+                .fsu("fhcm")
+                .status("planing")
+                .createBy("joe mama")
+                .location("joe mama house")
+                .duration(69)
+                .attendee("fresher")
+                .attendeePlanned(100)
+                .attendeeAccepted(99)
+                .attendeeActual(99)
+                .timeFrom(Time.valueOf("08:00:00"))
+                .timeTo(Time.valueOf("10:00:00"))
+                .startDate(new Date().getTime() - 1000000)
+                .endDate(new Date().getTime() + 2000000)
+                .trainingProgram("how to code with joe mama.")
+                .trainingProgramModifiedDate(new Date().getTime() - 5000000)
+                .listOfSyllabus(syllabusList)
+                .build();
+
+        return class_;
+    }
+
+    public GetClass getClass_(String id){
+        return GetClass.builder()
+                .className("class " + id)
+                .classCode(id)
+                .createdOn(new Date().getTime())
+                .fsu("fhcm")
+                .status("planing")
+                .createBy("joe mama")
+                .location("joe mama house")
+                .duration(69)
+                .build();
+    }
+
+
 }
